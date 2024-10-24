f95c0b2b430a91ace104d3a36e0ab37aeebcbd63
Oleksii Ruban <lierival@gmail.com>
2024-10-24 22:00:31 +0200

    Initial Commit

---
+++ TestGitHub/TestGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitHubApp.swift
+//  TestGitHub
+//
+//  Created by Oleksii Ruban on 24.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
