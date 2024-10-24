3d6568823485025df2f1a8867e0ebe39f706bea5
Oleksii Ruban <lierival@gmail.com>
2024-10-24 22:23:08 +0200

    [Upd] add TestDeleteView

---
+++ TestDeleteView.swift
@@ -0,0 +1,18 @@
+//
+//  TestDeleteView.swift
+//  TestGitHub
+//
+//  Created by Oleksii Ruban on 24.10.2024.
+//
+
+import SwiftUI
+
+struct TestDeleteView: View {
+    var body: some View {
+        Text("Delete me!")
+    }
+}
+
+#Preview {
+    TestDeleteView()
+}
