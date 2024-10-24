1311bc499d474c05487ddd08bfbae6c98ef67517
Oleksii Ruban <lierival@gmail.com>
2024-10-24 22:21:03 +0200

    [Upd] Delete ContentView

--- TestGitHub/ContentView.swift
+++ TestGitHub/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Add tet to tet brance")
+            Text("delete content view")
         }
         .padding()
     }
