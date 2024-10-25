765160352c379333d3f90fe3ce4c6f6d4e6dc64d
Oleksii Ruban <lierival@gmail.com>
2024-10-25 14:56:51 +0200

    add view

---
+++ TestGitHub/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  TestGitHub
+//
+//  Created by Oleksii Ruban on 25.10.2024.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
